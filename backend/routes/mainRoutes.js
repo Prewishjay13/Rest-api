2bbbc23c9f1dac9ae12cc896743b4e623aeea96e
Prewishjay13 <0935796@hr.nl>
2023-12-17 19:22:34 +0100

    .

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {getPosts, getPost, postRoutes, putRoutes, deleteRoutes} = require('../controllers/postController') 
+const {getPosts, getPost, postRoutes, putRoutes, deleteRoutes, optionsRoutes} = require('../controllers/postController') 
 
 router.route('/').get(getPosts).post(postRoutes);
 
 router.route('/:id').put(putRoutes).delete(deleteRoutes);
-
+router.route('/:id').options(optionsRoutes)
 router.route('/:id').get(getPost);
 module.exports = router 
