5329156a06c6b73535a357295ac516c019735293
Prewishjay13 <0935796@hr.nl>
2024-04-11 21:39:10 +0200

    .

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -158,7 +158,7 @@ router.post('/', async (req, res) => {
     headers['Access-Control-Allow-Origin'] = '*';
     headers['Content-Type'] = 'Content-Type', 'text/html; charset=UTF-8';
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
-    headers['Allow'] = 'GET, PUT, HEAD, DELETE, PATCH, OPTIONS';
+    headers['Allow'] = 'GET, PUT, DELETE, PATCH, OPTIONS';
     headers['Access-Control-Allow-Methods'] = 'GET, PUT, DELETE, Patch, OPTIONS';
     headers['Content-Length'] = '0';
     headers["Access-Control-Max-Age"] = '86400';
