9492e3d27f92bb61f7c044b8896194d20c75582e
Prewishjay13 <0935796@hr.nl>
2024-04-11 22:24:13 +0200

    Update postModel.js

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -13,24 +13,31 @@ const postSchema = new mongoose.Schema({
     address: {
         type: String,
         required: [true, 'Insert address']
-    }
+    },
+    _links:{
+        self:{
+            href:{
+                type: String
+            }
+        }, collection: {href: {type: String}}
+    }, 
 });
 
 // Set virtuals for toObject and toJSON
-postSchema.set('toObject', { virtuals: true });
-postSchema.set('toJSON', { virtuals: true });
+// postSchema.set('toObject', { virtuals: true });
+// postSchema.set('toJSON', { virtuals: true });
 
 // Add virtual properties to the model instance.
-postSchema.virtual('_links').get(function () {
-    return {
-        "self": {
-            "href": 'http://145.24.222.132:8000/posts' + this._id,
-        },
-        "collection": {
-            "href": 'http://145.24.222.132:8000/posts'
-        }
-    };
-});
+// postSchema.virtual('_links').get(function () {
+//     return {
+//         "self": {
+//             "href": 'http://145.24.222.132:8000/posts' + this._id,
+//         },
+//         "collection": {
+//             "href": 'http://145.24.222.132:8000/posts'
+//         }
+//     };
+// });
 
 // Export the model
 module.exports = mongoose.model('Post', postSchema);
