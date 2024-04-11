fe3bbb018cd7827d45b92a8476237d615392f894
Prewishjay13 <0935796@hr.nl>
2024-04-11 22:35:59 +0200

    Update postModel.js

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -24,7 +24,7 @@ const postSchema = new mongoose.Schema({
 });
 
 // Set virtuals for toObject and toJSON
-postSchema.set('toObject', { virtuals: true });
+//postSchema.set('toObject', { virtuals: true });
 postSchema.set('toJSON', { virtuals: true });
 
 // Add virtual properties to the model instance.
